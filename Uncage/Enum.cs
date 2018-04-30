68567ed4e6a870aff978615e0383a1ea0b15a341
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-04-30 10:48:29 +0200

    Added Enum class

---
+++ Uncage/Enum.cs
@@ -0,0 +1,12 @@
+﻿namespace Uncage
+{
+    public class Enum
+    {
+        public enum AuthError
+        {
+            SPOTIFY_ERROR = 0,
+            YOUTUBE_ERROR = 1,
+            UNKNOWN_ERROR = 2
+        }
+    }
+}
